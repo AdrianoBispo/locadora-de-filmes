cd772bf01c8861f6329a053ab48fff3154764714
AdrianoBispo <Drianvini10@gmail.com>
2022-08-11 22:48:46 -0300

    Adicionado os códigos da aula 9 do curso C# - Platzi.

--- Program.cs
+++ Program.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace curso_C_
-{
-    public class Teste
+namespace Locadora_Platzi
+{   
+    public class Program
     {
-        
+        public static void Main(string[] args)
+        {
+            string nome = "Matrix";
+            int anoLancamento = 1998;
+            string genero = "Ação";
+            float avaliacao = 9.5f;
+            int quantidade = 5;
+            bool lancamento = false;
+            
+            Console.WriteLine ("Seja Bem-Vindo à locadora Platzi!");
+            Console.WriteLine ("Por enquanto, este é o nosso único filme:");
+            
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Ano de Lançamento: " + anoLancamento);
+            Console.WriteLine("Gênero: " + genero);
+            Console.WriteLine("Avaliação: " + avaliacao);
+            Console.WriteLine("Quantidade: " + quantidade);
+            Console.WriteLine("Lançamento: " + lancamento);
+            
+            Console.ReadLine();
+            
+        }
     }
 }
